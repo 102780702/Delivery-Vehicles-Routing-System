22555f397015263d73baabdbbad57b8a130c034c
102780702 <102780702@students.swinburne.edu.my>
2024-04-07 20:28:30 +0800

    Major update - multi area rendering

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 let map;
+let batches = [];
+let currentIndex = 0;
+
+fetch('http://localhost:8080')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(map => {
+            let mapData = {};
+            for (const areaKey in map) 
+            {
+                if (Object.hasOwnProperty.call(map, areaKey)) 
+                {
+                    const areaData = map[areaKey];
+                    let area = [];
+                    areaData.forEach(coordinates => {
+                        let coordinateList = [];
+                        coordinates.forEach(coordinate => {
+                            // const jsonDataContainer = document.getElementById('jsonData');
+                            // jsonDataContainer.textContent = JSON.stringify(coordinate.longitude, null, 2); debug
+                            let coord = {lat: coordinate.latitude, lng: coordinate.longitude};
+                            coordinateList.push(coord);
+                        });
+                        area.push(coordinateList);
+                    });
+                        
+                    mapData[areaKey] = area;
+                }
+            }
+            batches.push(mapData);
+        });
+        initMap();
+    })
+    .catch(error => console.error('Error:', error));
 
 async function initMap() 
 {
@@ -15,19 +48,56 @@ async function initMap()
         mapId: "MAP_01",
     });
 
-    var points = 
-    [
-        {lat: 1.532302, lng: 110.357173},
-        {lat: 1.507103, lng: 110.360874},
-        {lat: 1.535546, lng: 110.358202},
-        {lat: 1.46668, lng: 110.425148},
-    ];
+    var batch = batches[currentIndex];
+    let Area_A = []
+    let Area_B = []
+    let Area_C = []
+    let Area_D = []
+
+    if (batch.hasOwnProperty('Area_A')) 
+    {
+        var areaAData = batch['Area_A'];
+
+        if (areaAData.length > 0) 
+        {
+            const coordinatesList = areaAData[0];
+            coordinatesList.forEach(coordinate => {
+                Area_A.push(coordinate);
+            });
+        }
+
+        // const jsonDataContainer = document.getElementById('jsonData');
+        // jsonDataContainer.textContent = JSON.stringify(Area_A, null, 2); debug
+    }
+
+    if (batch.hasOwnProperty('Area_B')) 
+    {
+        var areaAData = batch['Area_B'];
+
+        if (areaAData.length > 0) 
+        {
+            const coordinatesList = areaAData[0];
+            coordinatesList.forEach(coordinate => {
+                Area_B.push(coordinate);
+            });
+        }
+
+        // const jsonDataContainer = document.getElementById('jsonData');
+        // jsonDataContainer.textContent = JSON.stringify(Area_B, null, 2); debug
+    }
+
+    // CD
 
     var markerLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     
     function calculateRoute(index) 
     {
-        if (index >= points.length - 1) 
+        if (index >= Area_A.length - 1) 
+        {
+            return;
+        }
+
+        if (index >= Area_B.length - 1) 
         {
             return;
         }
@@ -43,13 +113,26 @@ async function initMap()
 
         // Marker setting
         new google.maps.Marker({
-            position: points[index],
+            position: Area_A[index],
             map: map,
             label: markerLetters[index % markerLetters.length]
         });
 
         new google.maps.Marker({
-            position: points[index + 1],
+            position: Area_A[index + 1],
+            map: map,
+            label: markerLetters[(index + 1) % markerLetters.length]
+        });
+
+        // Marker setting
+        new google.maps.Marker({
+            position: Area_B[index],
+            map: map,
+            label: markerLetters[index % markerLetters.length]
+        });
+
+        new google.maps.Marker({
+            position: Area_B[index + 1],
             map: map,
             label: markerLetters[(index + 1) % markerLetters.length]
         });
@@ -59,11 +142,13 @@ async function initMap()
         
         // Create a DirectionsService object.
         var directionsService = new google.maps.DirectionsService();
-
+        
         var request = 
         {
-            origin: points[index],
-            destination: points[index + 1],
+            origin: Area_A[index],
+            destination: Area_A[index + 1],
+            origin: Area_B[index],
+            destination: Area_B[index + 1],
             travelMode: google.maps.TravelMode.DRIVING
         };
     
@@ -78,17 +163,19 @@ async function initMap()
     }
     
     calculateRoute(0);
-
-    fetch('http://localhost:8080')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-
-            const jsonDataContainer = document.getElementById('jsonData');
-
-            jsonDataContainer.textContent = JSON.stringify(data, null, 2);
-        })
-        .catch(error => console.error('Error:', error));
 }
 
-initMap();
+// document.getElementById('yourButtonId').addEventListener('click', function() 
+// {
+//     currentIndex++;
+
+//     if (currentIndex >= batches.length) 
+//     {
+//         currentIndex = 0;
+//     }
+
+//     const currentBatch = batches[currentIndex];
+//     console.log('Current Batch:', currentBatch);
+
+//     // 在這裡你可以做任何其他你想做的事情，例如更新介面或處理數據等等
+// });
