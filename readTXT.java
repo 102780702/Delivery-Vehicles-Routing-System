ad3d11c5632e2c609764cafeb673610b95dccaf2
102780702 <102780702@students.swinburne.edu.my>
2024-04-26 14:00:16 +0800

    Area distribution

--- readTXT.java
+++ readTXT.java
@@ -3,6 +3,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 public class readTXT 
 {
@@ -18,6 +20,55 @@ public class readTXT
             System.out.println("Capacity: " + entry.getValue().getCapacity());
             System.out.println("Weight: " + entry.getValue().getWeight());
             System.out.println();
+        }
+
+        List<Coordinate> coordinateList = new ArrayList<>(coordinateInfoMap.keySet());
+        List<Coordinate> areaA = new ArrayList<>();
+        List<Coordinate> areaB = new ArrayList<>();
+        List<Coordinate> areaC = new ArrayList<>();
+        List<Coordinate> areaD = new ArrayList<>();
+
+    
+        Coordinate centerWarehouse = new Coordinate(1.532302, 110.357173);
+
+        for (Coordinate coordinate : coordinateList) 
+        {
+            if(coordinate.getLatitude() > centerWarehouse.getLatitude() && coordinate.getLongitude() < centerWarehouse.getLongitude())
+            {
+                areaA.add(coordinate);
+            }
+            else if(coordinate.getLatitude() > centerWarehouse.getLatitude() && coordinate.getLongitude() > centerWarehouse.getLongitude())
+            {
+                areaB.add(coordinate);
+            }
+            else if(coordinate.getLatitude() < centerWarehouse.getLatitude() && coordinate.getLongitude() < centerWarehouse.getLongitude())
+            {
+                areaC.add(coordinate);
+            }
+            else if(coordinate.getLatitude() < centerWarehouse.getLatitude() && coordinate.getLongitude() > centerWarehouse.getLongitude())
+            {
+                areaD.add(coordinate);
+            }
+        }
+
+        System.out.println("Area A:");
+        for (Coordinate coordinate : areaA) {
+            System.out.println("Latitude: " + coordinate.getLatitude() + ", Longitude: " + coordinate.getLongitude());
+        }
+
+        System.out.println("Area B:");
+        for (Coordinate coordinate : areaB) {
+            System.out.println("Latitude: " + coordinate.getLatitude() + ", Longitude: " + coordinate.getLongitude());
+        }
+
+        System.out.println("Area C:");
+        for (Coordinate coordinate : areaC) {
+            System.out.println("Latitude: " + coordinate.getLatitude() + ", Longitude: " + coordinate.getLongitude());
+        }
+
+        System.out.println("Area D:");
+        for (Coordinate coordinate : areaD) {
+            System.out.println("Latitude: " + coordinate.getLatitude() + ", Longitude: " + coordinate.getLongitude());
         }
     }
 
