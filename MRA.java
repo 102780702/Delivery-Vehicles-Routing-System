449a0516705726f5db47c889831f305f9e9eb703
102780702 <102780702@students.swinburne.edu.my>
2024-04-30 12:47:45 +0800

    Squashed commit of the following:
    commit f27832e57dd0b73c18e84dff273503699c86b44f
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Tue Apr 30 12:41:29 2024 +0800
    
        ready to combine (maybe)
    
    commit f11df9aad10f9a5df425648c5e0e54d88d274a3c
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Tue Apr 30 10:32:47 2024 +0800
    
        Adapting algorithm, almost there
    
    commit d4472a1f3ceab9df48973278bec6ca898dfd0a1c
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Mon Apr 29 17:59:05 2024 +0800
    
        combing...
    
    commit 42eab455e7545662780447deb9bb9d30f22536b0
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Mon Apr 29 15:41:57 2024 +0800
    
        combine
    
    commit 4c709dd5a3a0f5fbd6ddc3ef0b69165d0cf6958b
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Mon Apr 29 13:50:48 2024 +0800
    
        Update README.md
    
    commit 5b177fd726d8c7b9edddbde90fd651e5680dcfbe
    Merge: 838ec63 a582544
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Mon Apr 29 13:48:35 2024 +0800
    
        Merge branch 'junsieng' of https://github.com/102780702/Delivery-Vehicles-Routing-System into junsieng
    
    commit 838ec636a2cb4fd935f26212cd48e3233ef49227
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Mon Apr 29 13:47:55 2024 +0800
    
        Update README.md
    
    commit a58254478f8a9f9dc12ba2ed184bdfa30698a36a
    Author: 102780702 <145240138+102780702@users.noreply.github.com>
    Date:   Mon Apr 29 13:47:43 2024 +0800
    
        Update README.md
    
    commit 79300784e7e33a4be78f5c1366dd3b1c7143ca72
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Mon Apr 29 13:46:26 2024 +0800
    
        Create MRA.java
    
    commit 59e36b18460efe3f64870b1d126589f8652a69f0
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Fri Apr 26 15:59:09 2024 +0800
    
        code optimization
    
    commit 15e1a1d120c5eab2e2a278a19bfbf145257c6704
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Fri Apr 26 15:32:28 2024 +0800
    
        Area distribution ver2
    
    commit ad3d11c5632e2c609764cafeb673610b95dccaf2
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Fri Apr 26 14:00:16 2024 +0800
    
        Area distribution
    
    commit fad2f7f0bfb8bdae96ea9b359737a100e67b2ef2
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Fri Apr 26 11:39:58 2024 +0800
    
        tiny update
    
    commit 10fb5c020a6737fedd9584a501ccf7e5c59da9b7
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Thu Apr 25 10:49:27 2024 +0800
    
        tiny update
    
    commit 02c8b7139dd0bb82c89eaab192a7b770527c78dd
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Thu Apr 25 08:41:11 2024 +0800
    
        Capacity & weight added
    
    commit 25eec5bb4c8062312f8e2ba2e8c081b20b6116ca
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Wed Apr 24 11:32:08 2024 +0800
    
        delete test
    
    commit 0dc6111623f778ff736f12d37a3aae50abe13202
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Tue Apr 23 17:26:30 2024 +0800
    
        Testing purpose
    
    commit 1354948522d45e746c21839e6987afb04cc93b91
    Author: 102780702 <102780702@students.swinburne.edu.my>
    Date:   Mon Apr 8 11:48:55 2024 +0800
    
        Tiny changes

---
+++ MRA.java
@@ -0,0 +1,84 @@
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.Serializable;
+
+public class MRA extends Agent implements Serializable {
+	private static final long serialVersionUID = 1L;
+    private Map<String, Integer> capacityMap; // Map to store the capacities of delivery agents
+    
+    protected void setup() {
+        capacityMap = new HashMap<>();
+        
+        // Add behavior to handle incoming messages
+        addBehaviour(new HandleRequests());
+    }
+    
+    private class HandleRequests extends CyclicBehaviour implements Serializable {
+    	private static final long serialVersionUID = 1L;
+        public void action() {
+            // Define the message template to match incoming requests
+            MessageTemplate template = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+            
+            // Receive the request message
+            ACLMessage request = receive(template);
+            
+            if (request != null) {
+                // Step 1: MRA gets parcel information from the database
+                String parcelInfo = getParcelInfoFromDatabase();
+                
+                // Step 2: MRA sends a request to DA to request to send parcel
+                ACLMessage sendParcelRequest = new ACLMessage(ACLMessage.REQUEST);
+                sendParcelRequest.setContent(parcelInfo);
+                sendParcelRequest.addReceiver(request.getSender());
+                send(sendParcelRequest);
+                
+                // Step 3: The DA requests route from MRA (implicit in the request from DA)
+                
+                // Step 4: The DA informs its capacity to MRA
+                capacityMap.put(request.getSender().getLocalName(), Integer.parseInt(request.getContent()));
+                
+                // Step 5: Perform Genetic Algorithm to create the route
+                String optimizedRoute = performGeneticAlgorithm();
+                
+                // Step 5: The MRA informs route to DA
+                ACLMessage informRoute = new ACLMessage(ACLMessage.INFORM);
+                informRoute.setContent(optimizedRoute);
+                informRoute.addReceiver(request.getSender());
+                send(informRoute);
+                
+                // Wait for response from DA
+                ACLMessage response = blockingReceive(MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+                
+                if (response != null) {
+                    // Step 6: The DA informs MRA that it has completed the task
+                    if (response.getContent().equals("Task Completed")) {
+                        System.out.println("Delivery agent " + response.getSender().getLocalName() + " has completed the task.");
+                    } else {
+                        System.out.println("Delivery agent " + response.getSender().getLocalName() + " failed to complete the task.");
+                    }
+                } else {
+                    System.out.println("No response from delivery agent.");
+                }
+            }
+            else {
+                // If no message received, block the behavior
+                block();
+            }
+        }
+    }
+    
+    private String getParcelInfoFromDatabase() {
+        // Implement this method to retrieve parcel information from the database
+        // For demonstration purposes, a placeholder string is returned
+        return "Parcel information from the database";
+    }
+    
+    private String performGeneticAlgorithm() {
+        return "route";
+    }
+}
+
--- readTXT.java
+++ readTXT.java
@@ -8,6 +8,82 @@ import java.util.ArrayList;
 
 public class readTXT 
 {
+    private static int calculateDistance(Coordinate loc1, Coordinate loc2) {
+        double lat1 = Math.toRadians(loc1.getLatitude());
+        double lon1 = Math.toRadians(loc1.getLongitude());
+        double lat2 = Math.toRadians(loc2.getLatitude());
+        double lon2 = Math.toRadians(loc2.getLongitude());
+        
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        
+        double a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        
+        // earth radius in meters
+        double radius = 6371000; // 6371 km converted to meters
+        
+        return (int) Math.round(radius * c);
+    }
+    
+
+    public static List<Integer> calculateDistances(List<Coordinate> locations) {
+        List<Integer> distances = new ArrayList<>();
+        int n = locations.size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Coordinate loc1 = locations.get(i);
+                Coordinate loc2 = locations.get(j);
+                int distance = calculateDistance(loc1, loc2);
+                distances.add(distance);
+            }
+        }
+        
+        return distances;
+    }
+
+    public static void printDistanceReference(List<Coordinate> locations, List<Integer> distances) {
+        int n = locations.size();
+        int index = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Coordinate loc1 = locations.get(i);
+                Coordinate loc2 = locations.get(j);
+                System.out.println("[ " +loc1.getLatitude() + ", " + loc1.getLongitude() + " ] and [" +loc2.getLatitude() + ", " + loc2.getLongitude() + "]: " + distances.get(index).toString() + "m");
+                index++;
+            }
+        }
+    }
+
+    public static void printTravelPrices(int[][] travelPrices, int numberOfCoordinates){
+        for(int i = 0; i<numberOfCoordinates; i++){
+            for(int j=0; j<numberOfCoordinates; j++){
+                System.out.print(travelPrices[i][j]);
+                if(travelPrices[i][j]/10 == 0)
+                    System.out.print("  ");
+                else
+                    System.out.print(' ');
+            }
+            System.out.println("");
+        }
+    }
+
+    public static List<Integer> convertSalesmanGenomeToIntList(SalesmanGenome genome) {
+        List<Integer> integerSequence = new ArrayList<>();
+        
+        integerSequence.add(genome.getStartingCity());
+        
+        for (int gene : genome.getGenome()) {
+            integerSequence.add(gene);
+        }
+        
+        integerSequence.add(genome.getStartingCity());
+        
+        return integerSequence;
+    }
+
     public static void main(String[] args) 
     {
         String fileName = "coordinates.txt";
@@ -22,66 +98,92 @@ public class readTXT
         //     System.out.println();
         // }
 
-        Map<Coordinate, CoordinateInfo> areaA = new HashMap<>();
-        Map<Coordinate, CoordinateInfo> areaB = new HashMap<>();
-        Map<Coordinate, CoordinateInfo> areaC = new HashMap<>();
-        Map<Coordinate, CoordinateInfo> areaD = new HashMap<>();
-
+        List<Coordinate> coordinateList = new ArrayList<>(coordinateInfoMap.keySet());
         Coordinate centerWarehouse = new Coordinate(1.532302, 110.357173);
+        coordinateList.add(0, centerWarehouse);
+        
+        // for (Coordinate coordinate : coordinateList) {
+        //     System.out.println("Coordinate: " + coordinate.getLatitude());
+        // }
 
-        for (Map.Entry<Coordinate, CoordinateInfo> entry : coordinateInfoMap.entrySet())
-        {
-            if(entry.getKey().getLatitude() > centerWarehouse.getLatitude() && entry.getKey().getLongitude() < centerWarehouse.getLongitude())
-            {
-                areaA.put(entry.getKey(), entry.getValue());
-            }
-            else if(entry.getKey().getLatitude() > centerWarehouse.getLatitude() && entry.getKey().getLongitude() > centerWarehouse.getLongitude())
-            {
-                areaB.put(entry.getKey(), entry.getValue());
-            }
-            else if(entry.getKey().getLatitude() < centerWarehouse.getLatitude() && entry.getKey().getLongitude() < centerWarehouse.getLongitude())
-            {
-                areaC.put(entry.getKey(), entry.getValue());
-            }
-            else if(entry.getKey().getLatitude() < centerWarehouse.getLatitude() && entry.getKey().getLongitude() > centerWarehouse.getLongitude())
-            {
-                areaD.put(entry.getKey(), entry.getValue());
+        List<Integer> distances = calculateDistances(coordinateList);
+        printDistanceReference(coordinateList, distances);
+
+        // int count = 1;
+        // for (int distance : distances) {
+        //     System.out.println("Distance " + count + ": " + distance + " km");
+        //     count++;
+        // }
+
+        int numberOfCoordinates = coordinateList.size();
+        int[][] travelPrices = new int[numberOfCoordinates][numberOfCoordinates];
+        int index = 0;
+
+        for (int i = 0; i < numberOfCoordinates; i++) {
+            for (int j = 0; j < numberOfCoordinates; j++) {
+                if (i != j) {
+                    if (travelPrices[i][j] == 0) {
+                        travelPrices[i][j] = distances.get(index);
+                        travelPrices[j][i] = travelPrices[i][j];
+                        index ++;
+                    }
+                }
             }
         }
-        
-        areaA.forEach((coordinate, info) -> {
-            System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
-        });
-        areaB.forEach((coordinate, info) -> {
-            System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
-        });
-        areaC.forEach((coordinate, info) -> {
-            System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
-        });
-        areaD.forEach((coordinate, info) -> {
-            System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
-        });
 
-        // for (Map.Entry<Coordinate, CoordinateInfo> entry : areaA.entrySet()) {
-        //     Coordinate coordinate = entry.getKey();
-        //     CoordinateInfo info = entry.getValue();
-        //     System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
+        printTravelPrices(travelPrices,numberOfCoordinates); // print matrix
+
+        UberSalesmensch geneticAlgorithm = new UberSalesmensch(numberOfCoordinates, SelectionType.ROULETTE, travelPrices, 0, 0);
+        SalesmanGenome result = geneticAlgorithm.optimize();
+        System.out.println(result);
+        List<Integer> resultList = convertSalesmanGenomeToIntList(result);
+        System.out.println(resultList);
+
+        List<Coordinate> shortestPath = new ArrayList<>();        ;
+
+        for (int i: resultList) {
+            shortestPath.add(coordinateList.get(i));
+        }
+
+        // Map<Coordinate, CoordinateInfo> areaA = new HashMap<>();
+        // Map<Coordinate, CoordinateInfo> areaB = new HashMap<>();
+        // Map<Coordinate, CoordinateInfo> areaC = new HashMap<>();
+        // Map<Coordinate, CoordinateInfo> areaD = new HashMap<>();
+
+        // Coordinate centerWarehouse = new Coordinate(1.532302, 110.357173);
+
+        // for (Map.Entry<Coordinate, CoordinateInfo> entry : coordinateInfoMap.entrySet())
+        // {
+        //     if(entry.getKey().getLatitude() > centerWarehouse.getLatitude() && entry.getKey().getLongitude() < centerWarehouse.getLongitude())
+        //     {
+        //         areaA.put(entry.getKey(), entry.getValue());
+        //     }
+        //     else if(entry.getKey().getLatitude() > centerWarehouse.getLatitude() && entry.getKey().getLongitude() > centerWarehouse.getLongitude())
+        //     {
+        //         areaB.put(entry.getKey(), entry.getValue());
+        //     }
+        //     else if(entry.getKey().getLatitude() < centerWarehouse.getLatitude() && entry.getKey().getLongitude() < centerWarehouse.getLongitude())
+        //     {
+        //         areaC.put(entry.getKey(), entry.getValue());
+        //     }
+        //     else if(entry.getKey().getLatitude() < centerWarehouse.getLatitude() && entry.getKey().getLongitude() > centerWarehouse.getLongitude())
+        //     {
+        //         areaD.put(entry.getKey(), entry.getValue());
+        //     }
         // }
-        // for (Map.Entry<Coordinate, CoordinateInfo> entry : areaB.entrySet()) {
-        //     Coordinate coordinate = entry.getKey();
-        //     CoordinateInfo info = entry.getValue();
+        
+        // areaA.forEach((coordinate, info) -> {
         //     System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
-        // }
-        // for (Map.Entry<Coordinate, CoordinateInfo> entry : areaC.entrySet()) {
-        //     Coordinate coordinate = entry.getKey();
-        //     CoordinateInfo info = entry.getValue();
+        // });
+        // areaB.forEach((coordinate, info) -> {
         //     System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
-        // }
-        // for (Map.Entry<Coordinate, CoordinateInfo> entry : areaD.entrySet()) {
-        //     Coordinate coordinate = entry.getKey();
-        //     CoordinateInfo info = entry.getValue();
+        // });
+        // areaC.forEach((coordinate, info) -> {
         //     System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
-        // }
+        // });
+        // areaD.forEach((coordinate, info) -> {
+        //     System.out.println("Coordinate: " + coordinate + ", Capacity: " + info.getCapacity() + ", Weight: " + info.getWeight());
+        // });
     }
 
     // read file function
